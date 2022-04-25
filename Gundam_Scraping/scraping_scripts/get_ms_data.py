9835a094a80343caa0d64fe2c506545ba9eb6e6a
Multach <matt.multach87@gmail.com>
2022-04-25 16:02:40 -0400

    -Initial updates of AC scraper to fit Gundam wiki:   -->Replace 'villager' with 'ms'   -->Replace wiki_url with gundam wiki url

--- Gundam_Scraping/scraping_scripts/get_ms_data.py
+++ Gundam_Scraping/scraping_scripts/get_ms_data.py
@@ -4,28 +4,27 @@ from requests import get
 from bs4 import BeautifulSoup
 from urllib import request
 
-wiki_url = "https://animalcrossing.fandom.com/wiki/"
+wiki_url = "https://gundam.fandom.com/wiki/The_Gundam_Wiki"
 
 
-def get_villager_data(villager_name: str):
+def get_ms_data(ms_name: str):
     """
-    Scrape all the data for a given villager from the Animal Crossing Fandom wiki.
-    Also saves the image associated with a given villager in a folder
-    :param villager_name: the name of the villager, i.e. "Zucker"
-    :return: A dictionary of all the data scraped for the respective villager
+    Scrape all the data for a given ms from the Gundam Fandom wiki.
+    :param ms_name: the name of the ms, i.e. "Zucker"
+    :return: A dictionary of all the data scraped for the respective ms
     """
 
     # Get the page and parse it with BeautifulSoup
-    response = get(wiki_url + villager_name)
+    response = get(wiki_url + ms_name)
     soup_response = BeautifulSoup(response.text, 'html.parser')
 
     # Will store the data as a dictionary and write it as JSON later
-    villager_data = {}
+    ms_data = {}
 
-    # The "aside" is an HTML element containing all the important villager information
+    # The "aside" is an HTML element containing all the important ms information
     aside = soup_response.findAll("aside")[0]
 
-    # Get all the data for this villager
+    # Get all the data for this ms
     names = aside.find_all("h2", {"class": "pi-item pi-item-spacing pi-title"})
     figure = aside.find_all("figure", {"class": "pi-item pi-image"})[0]
     attributes = aside.find_all("div", {"class": "pi-item pi-data pi-item-spacing pi-border-color"})
@@ -33,16 +32,16 @@ def get_villager_data(villager_name: str):
     # Clean and store names
     cleaned_names = [name.getText() for name in names]
 
-    villager_data["name_en"] = cleaned_names[0]
+    ms_data["name_en"] = cleaned_names[0]
     if len(cleaned_names) > 1:
-        villager_data["name_jp"] = cleaned_names[1]
+        ms_data["name_jp"] = cleaned_names[1]
 
     # Clean and store the figure data
     high_res_image_url = figure.find_all("a")[0].get("href")
     caption = figure.find_all("figcaption")
-    urllib.request.urlretrieve(high_res_image_url, "villager-data/images/" + villager_name + ".jpg")
+    urllib.request.urlretrieve(high_res_image_url, "ms-data/images/" + ms_name + ".jpg")
     if len(caption) > 0:
-        villager_data["caption"] = caption[0].getText()
+        ms_data["caption"] = caption[0].getText()
 
     # Clean and store the attribute data
     for attribute in attributes:
@@ -65,6 +64,6 @@ def get_villager_data(villager_name: str):
             if "," in values:
                 values = [v.strip() for v in values.split(",")]
 
-        villager_data[key.lower()] = values
+        ms_data[key.lower()] = values
 
-    return villager_data
+    return ms_data
