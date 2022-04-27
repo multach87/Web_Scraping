3c80e7607d10bfc9d89c598b1ec18e614d3b2123
Multach <matt.multach87@gmail.com>
2022-04-27 11:58:31 -0400

    -Updates to main_scrape.py while working on   get_ms_data_testing.py

--- Gundam_Scraping/main_scraper.py
+++ Gundam_Scraping/main_scraper.py
@@ -1,16 +1,16 @@
 import json
 from os import mkdir
 
-from get_villager_list import get_villager_list
-from get_villager_data import get_villager_data
+from get_ms_list import get_ms_list
+from get_ms_data import get_ms_data
 
 # Get our villages
 
 print("Getting the list of villagers to scrape...", end="")
-villager_list = get_villager_list()
+ms_list = get_ms_list()
 
 # Couldn't locate the table
-if len(villager_list) == 0:
+if len(ms_list) == 0:
     print("\nUh-oh, couldn't locate the table.\n\n" +
           "Maybe the wiki has changed, and broken this scraper.\n" +
           "In any case, please make an issue and I'll try to fix it!")
@@ -30,18 +30,18 @@ except FileExistsError:
           "Delete it and try again, if you want to re-download the images.\n")
 
 # Get the data for each villager
-total_villager_data = {}
-for villager in villager_list:
+total_ms_data = {}
+for villager in ms_list:
 
-    villager_local_name = villager.replace("/wiki/",  "")
+    ms_local_name = villager.replace("/wiki/",  "")
 
-    print("Getting data on", villager_local_name + "...", end="")
-    villager_data = get_villager_data(villager_local_name)
+    print("Getting data on", ms_local_name + "...", end="")
+    ms_data = get_ms_data(ms_local_name)
     print("Done")
 
-    total_villager_data[villager_local_name] = villager_data
+    total_ms_data[ms_local_name] = ms_data
 
 print("\nSaving villager JSON data...")
-with open("villager-data/villager-data.json", "w") as villager_data_file:
-    json.dump(total_villager_data, villager_data_file, indent=4)
+with open("villager-data/villager-data.json", "w") as ms_data_file:
+    json.dump(total_ms_data, ms_data_file, indent=4)
 print("Done")
