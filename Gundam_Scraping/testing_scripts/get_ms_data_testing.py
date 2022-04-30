df5f264fe435bd87f3952e9ff9e406275bb8c14e
Multach <matt.multach87@gmail.com>
2022-04-29 21:45:00 -0400

    -Updates to get_ms_data_testing.py: -->Cleaned up code -->Added documentation/comments -->Changed armament numbers to ints ---->Try-Except for non-int corner cases -->Tried to handle trailing spaces. Needs more work ---->Also problem of "\u200e" for some trailing space values -->.lower()'ed attribute values except urls

--- Gundam_Scraping/testing_scripts/get_ms_data_testing.py
+++ Gundam_Scraping/testing_scripts/get_ms_data_testing.py
@@ -14,9 +14,20 @@ wiki_url = "https://gundam.fandom.com"
 
 def get_mw_data(ms_name: str):
     """
-    Scrape all the data for a given ms from the Gundam Fandom wiki.
-    :param ms_name: the name of the ms, i.e. "Zucker"
-    :return: A dictionary of all the data scraped for the respective ms
+    Scrape all the data for a given mw from the Gundam Fandom wiki.
+    :param ms_name: the url-ized version of the mw name, \
+        e.g., GX-9900-DV_Gundam_X_Divider
+    :return: A dictionary of all the data scraped for the respective ms\
+        -All attributes are pre-pended with "SectionHeader_"
+        -All section names and attribute names have spaces removed
+        -Measurements are separated into 2-item lists with float for the value\
+            and string for the unit of measurement
+        -All Armament attributes are 2-item lists:
+            -List of armaments (string)
+            -List of floats corresponding to each\
+                armament in first string list
+                e.g., [['Beam Sword', 'Breast Vulcan'], [2, 2]]\
+                    indicates an mw has 2 beam swords and 2 breast vulcans
     """
 
     # Get the page and parse it with BeautifulSoup
@@ -26,125 +37,134 @@ def get_mw_data(ms_name: str):
     # Will store the data as a dictionary and write it as JSON later
     mw_data = {}
 
-    # The "aside" is an HTML element containing all the important ms information
+    # The "aside" is an HTML element containing all the important mw information
     aside = soup_response.findAll("aside")[0]
 
-    # initialize attribute keys and values
+    # initialize lists of attribute keys and values
     attrs_key = []
     attrs_val = []
 
-    # Get all the data for this ms
+    # Get names and types for mw (i.e., the single-item sections)
     names = aside.find_all("h2", {"class": \
         "pi-item pi-item-spacing pi-title pi-secondary-background"})
     types = aside.find_all("td")
-    """attributes = aside.find_all("div", {"class": \
-        "pi-item pi-data pi-item-spacing pi-border-color"})"""
 
     # Clean and store name
     cleaned_names = [name.getText() for name in names]
-    # mw_data["name_en"] = cleaned_names[0]
-    attrs_valname = []
+    # # Add "name" to keys list
     attrs_key.append("names")
-    attrs_valname.append(cleaned_names[0])
+    # # Initialize value list for names
+    attrs_valname = []
+    # # Store english name
+    attrs_valname.append(cleaned_names[0].lower())
+    # # Check for japanese name and add to names value list
     if(len(cleaned_names) > 1):
-        # mw_data["name_jp"] = cleaned_names[1].split(" (")[1].split(")")[0]
-        # attrs_key.append("name_jp")
         attrs_valname.append(cleaned_names[1].split(" (")[1].split(")")[0])
+    # # Add list of name values to values list
     attrs_val.append(attrs_valname)
     
     # Clean and store type
     cleaned_type = [type.getText() for type in types]
-    attrs_valtype = []
-    # mw_data["type_main"] = cleaned_type[0].split(" ")[-2] + " " + cleaned_type[0].split(" ")[-1]
+    # # Add "type" to keys list
     attrs_key.append("type")
-    attrs_valtype.append(cleaned_type[0].split(" ")[-2] + " " + cleaned_type[0].split(" ")[-1])
-    # mw_data["type_extra"] = ' '.join(cleaned_type[0].split(" ")[:-2]).split("\t\t\t\t")[1]
-    attrs_valtype.append(' '.join(cleaned_type[0].split(" ")[:-2]).split("\t\t\t\t")[1])
+    # # Initialize value list for type
+    attrs_valtype = []
+    # # Store main value for type
+    attrs_valtype.append((cleaned_type[0].split(" ")[-2] + \
+        " " + cleaned_type[0].split(" ")[-1]).lower())
+    # # Store type qualifier value
+    attrs_valtype.append((' '.join(cleaned_type[0].\
+        split(" ")[:-2]).split("\t\t\t\t")[1]).lower())
+    # # Add list of type values to values list
     attrs_val.append(attrs_valtype)
 
-    # Locate and store image urls, captions
-    # # Initialize lists
-    img_vals = []
+    # Clean and store image urls, image captions
+    # # Add "imgs" to keys list
+    attrs_key.append("imgs")
+    # # Initialize lists of captions and urls
+    img_urls = []
+    img_capts = []
     # # Append each link, caption to list
     for link in aside.find_all("a", {"class": \
         "image image-thumbnail"}):
         l = link.get("href")
         c = link.get("title")
-        img_vals.append(c)
-        img_vals.append(l)
-    # # Zip captions and links into dictionary
-    # mw_data["imgs"] = dict(zip(img_capts, img_urls))
-    attrs_key.append("imgs")
-    attrs_val.append(img_vals)
+        img_urls.append(l)
+        img_capts.append(c)
+    # # Add list of image urls and list of cpations to values list
+    attrs_val.append([img_urls, img_capts])
 
     # Clean and store the attribute data
+    # # Loop over all mw attribute sections except first \
+    # # (which is covered in the "type" section)
+    for sections in aside.findAll(["section", \
+        {"class": "pi-item pi-group pi-border-color"}, \
+        "section", {"class": \
+            "pi-item pi-group pi-border-color pi-collapse pi-collapse-closed"}])[1:]:
+        
+        # # # Store all section headers in list
+        h2 = sections.findAll("h2", {"class": \
+            "pi-item pi-header pi-secondary-font pi-item-spacing pi-secondary-background"})\
+                [0].getText()
+        
+        # # # Loop over all items within each section
+        for attribute in sections.find_all("div", {"class": \
+            "pi-item pi-data pi-item-spacing pi-border-color"}):
+            
+            # # # # initialize values list for attributes with multiple values
+            attrs_val2 = []
+
+            # # # # store attribute name/key
+            key = ''.join(h2.split(" ")) + "_" + \
+                ''.join(attribute.find_all("h3")[0].getText().split(" "))
+
+            # # # # for handling Overall Height in multiple measurement units
+            for value in attribute.find_all("li"):
+                """if value.find_all("span", {"class": "plainlinks"}):
+                    attrs_val2 = [''.join(value.getText().split())]"""
+                
+                if value.findChildren("span", {"class": "smwtext"}):
+                    attrs_val2.append(value.findChildren("span", {"class": \
+                        "smwtext"})[0].getText().lower())
+                else:
+                    attrs_val2.append(value.getText().lower())
+                    # attrs_val2 = [''.join(value.getText().split())]
+            
+            # # # # store attribute value
+            value = attribute.find_all("div")[0]
+            
+            # # # # update keys and values
+            attrs_key.append(key)
+            attrs_val.append(attrs_val2)
+
+    # zip together attribute keys and values into dictionary
+    mw_data = dict(zip(attrs_key, attrs_val))
+    
+    # Clean measurements and armament numbers
     # # List of measurement words (used later)
     measures = ['Height', 'Weight', 'PowerOutput', \
         'SensorRange', 'Acceleration', 'Speed']
-    """    # # initialize attribute keys and values
-        attrs_key = []
-        attrs_val = []"""
-    
-    # # extract info from each attribute
-    for sections in aside.findAll(["section", \
-        {"class": "pi-item pi-group pi-border-color"}, \
-        "section", \
-            {"class": "pi-item pi-group pi-border-color pi-collapse pi-collapse-closed"}])[1:]:
-        h2 = sections.findAll("h2", {"class": \
-            "pi-item pi-header pi-secondary-font pi-item-spacing pi-secondary-background"})[0].getText()
-        for attribute in sections.find_all("div", {"class": \
-            "pi-item pi-data pi-item-spacing pi-border-color"}):
-            # # # initialize values list for attributes with multiple values
-            attrs_val2 = []
-
-            # # # store attribute name/key
-            """key = print(''.join(h2.split(" ")) + "_" + \
-                ''.join(attribute.find_all("h3")[0].getText().split(" ")))"""
-            key = ''.join(attribute.find_all("h3")[0].getText().split(" "))
-            key2 = ''.join(h2.split(" ")) + "_" + \
-                ''.join(attribute.find_all("h3")[0].getText().split(" "))
-
-            # # # for handling Overall Height in multiple measurement units
-            for value in attribute.find_all("li"):
-                if value.findChildren("span", {"class": "smwtext"}):
-                    attrs_val2.append(value.findChildren("span", {"class": \
-                        "smwtext"})[0].getText())
-                else:
-                    attrs_val2.append(value.getText())
-            
-            # # # store attribute value
-            value = attribute.find_all("div")[0]
-            
-            # # # update keys and values
-            attrs_key.append(key2)
-            attrs_val.append(attrs_val2)
-        # # zip together attribute key and value into dictionary
-    mw_data = dict(zip(attrs_key, attrs_val))
     # # Separate measurements from units, armament numbers from armament
     for dicts in mw_data:
+        # # # Separate measurements from untis
         if any(ele in dicts for ele in measures):
-            """units = []
-            nums = []"""
             mw_data[dicts] = mw_data[dicts][0].split(" ", 1)
             mw_data[dicts][0] = float(mw_data[dicts][0])
+        # # # Separate armaments from armament numbers
         if 'Armament' in dicts:
             arms = []
             nums = []
             for arm in mw_data[dicts]:
                 if ' x ' in arm:
                     arms.append(arm.split(" x ", 1)[1])
-                    nums.append(float(arm.split(" x ", 1)[0]))
+                    try:
+                        nums.append(int(arm.split(" x ", 1)[0]))
+                    except:
+                        nums.append(arm.split(" x ", 1)[0])
                 else:
                     arms.append(arm)
                     nums.append(1)
-            mw_data[dicts] = [nums, arms]
-
-    """if ' x ' in mw_data[dicts]:
-                mw_data[dicts] = mw_data[dicts].split(" x ", 1)
-            else:
-                mw_data[dicts] = ['1', mw_data[dicts]]"""
-
-    # print(mw_data)
+            mw_data[dicts] = [arms, nums]
 
     return mw_data
 
@@ -171,6 +191,15 @@ for mw in mw_list:
 
 # Old/not functional stuff
 # Clean and store figures
+"""if ' x ' in mw_data[dicts]:
+            mw_data[dicts] = mw_data[dicts].split(" x ", 1)
+        else:
+            mw_data[dicts] = ['1', mw_data[dicts]]"""
+
+# print(mw_data)
+"""attributes = aside.find_all("div", {"class": \
+    "pi-item pi-data pi-item-spacing pi-border-color"})"""
+
 """figs_url = []
 figs_capt = []
 for link in figures.find_all("a"):
