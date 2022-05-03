39cf1ac815ac1086d783a3d15af1255dad031140
Multach <matt.multach87@gmail.com>
2022-05-03 13:15:40 -0400

    Multiple updates: -->Added traceback, timit imports -->Added and fixed subdirectory imports for get_list and get_data -->Incorporated successful changes from testing version -->Added "__main__" section

--- Gundam_Scraping/main_scraper.py
+++ Gundam_Scraping/main_scraper.py
@@ -1,47 +1,60 @@
 import json
 from os import mkdir
+import traceback
+import timeit
 
-# from get_mw_list import get_mw_list
-# from get_mw_data import get_mw_data
+from scraping_scripts.get_mw_list import get_list
+from scraping_scripts.get_mw_data import get_data
 
-# Get our villages
+if __name__ == "__main__":
 
-print("Getting the list of villagers to scrape...", end="")
-mw_list = get_mw_list()
+    # start timer
+    start = timeit.default_timer()
 
-# Couldn't locate the table
-if len(mw_list) == 0:
-    print("\nUh-oh, couldn't locate the table.\n\n" +
-          "Maybe the wiki has changed, and broken this scraper.\n" +
-          "In any case, please make an issue and I'll try to fix it!")
-    exit(0)
+    # get list of mw's
+    print("Getting the list of mw's to scrape...", end="")
+    mw_list = get_list()
 
-print("Done")
+    # Couldn't locate the table
+    if len(mw_list) == 0:
+        print("\nUh-oh, couldn't locate the table.\n\n" +
+            "Maybe the wiki has changed, and broken this scraper.\n" +
+            "In any case, please make an issue and I'll try to fix it!")
+        exit(0)
 
-# Create directory to store images
-try:
-    mkdir("mw-data")
-    print("\nCreated directory (mw-data) for mw data")
-
-    mkdir("mw-data/images")
-    print("Created directory (mw-data/images) for mw images\n")
-except FileExistsError:
-    print("\nDirectory already exists for mw data.\n" +
-          "Delete it and try again, if you want to re-download the images.\n")
-
-# Get the data for each mw
-total_mw_data = {}
-for mw in mw_list:
-
-    mw_local_name = mw.replace("/wiki/",  "")
-
-    print("Getting data on", mw_local_name + "...", end="")
-    mw_data = get_mw_data(mw_local_name)
     print("Done")
 
-    total_mw_data[mw_local_name] = mw_data
+    # Create directories for data storage
+    try:
+        mkdir("Data/mw_data")
+        print("\nCreated directory (mw_data) for mw data")
 
-print("\nSaving villager JSON data...")
-with open("villager-data/villager-data.json", "w") as mw_data_file:
-    json.dump(total_mw_data, mw_data_file, indent=4)
-print("Done")
+        mkdir("Data/images")
+        print("Created directory (Data/images) for mw images\n")
+    except FileExistsError:
+        print("\nDirectory already exists for mw data.\n" +
+            "Delete it and try again, if you want to re-download the images.\n")
+
+    # Get the data for each mw
+    total_mw_data = {}
+    total_mw_errors = {}
+    for mw in mw_list:
+
+        print("Getting data on", mw + "...", end="")
+        try:
+            mw_data = get_data(mw)
+            total_mw_data[mw] = mw_data
+        except:
+            mw_data = [traceback.format_exc()]
+            total_mw_errors[mw] = mw_data
+        print("Done")
+
+    print("\nSaving mw JSON data...")
+    with open("Data/mw_data/mw_data.json", "w") as mw_data_file:
+        json.dump(total_mw_data, mw_data_file, indent=4)
+    with open("Data/mw_data/mw_errors.json", "w") as total_mw_errors_file:
+        json.dump(total_mw_errors, total_mw_errors_file, indent=4)
+    print("Done")
+
+    stop = timeit.default_timer()
+    print ('Time: ', stop - start)
