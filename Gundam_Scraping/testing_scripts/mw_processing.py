eeccb326745ec7c8758f36132e6314146245ded2
Multach <matt.multach87@gmail.com>
2022-05-08 16:46:46 -0400

    Multiple updates: -Define one_hot function for one-hot encoding -->Yes, because of the input format, I can't just use ---->a normal one-hot function -Completed statements for iteratively one-hotting -->Keys that aren't names, types, imgs, or numbered ---->characteristics like equipment -OVA's incorporated into episode splitting/stripping

--- Gundam_Scraping/testing_scripts/mw_processing.py
+++ Gundam_Scraping/testing_scripts/mw_processing.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+import numpy as np
 
 def get_main(mw_name: str):
     data_temp = data[mw_name]
@@ -26,6 +27,16 @@ def get_main(mw_name: str):
     new[str(''.join(new["type"][0].split(" ")))] = 1
     return(new)
 
+def one_hot(key_name: str):
+    data_temp = pd.DataFrame.from_dict(data_1mw[key_name])
+    key_temp = key_name.split("_" , 1)[1]
+    data_temp = data_temp[0].str.get_dummies().add_prefix((key_temp + "_"))
+    data_temp.columns = data_temp.columns.str.replace(' ', '').str.replace('[', '_').str.replace(']', '').str.replace(':', '').str.replace('\'', '')
+    data_temp = data_temp.drop(data_temp.index[1:])
+    data_temp[0:] = 1
+    return(data_temp)
+
+
 # vars
 """data["RX-78-2_Gundam"]["names"][0]
 data["RX-78-2_Gundam"]["type"][0]"""
@@ -39,49 +50,38 @@ if __name__ == "__main__":
     #data_1mw = data["Pyrennes-class"]
     
     # Step 1: extact main info (full name, designation, model_series, type, and one-hots)
-    """datatest = get_main("RX-78-2_Gundam")
-    print(datatest)""" 
-
-    # One-hot encoding stuff: TOBE looped
-    """newnew = pd.DataFrame.from_dict(data["Profile_KnownPilots"])
-    #newnew.columns = [""]
-    #print(newnew)
-    #print(newnew[0])
-    newnew = newnew[0].str.get_dummies().add_prefix('Pilots_')
-    newnew.columns = newnew.columns.str.replace(' ', '').str.replace("[", '_').str.replace(']', '')
-    newnew = newnew.drop(newnew.index[1:])
-    newnew[0:] = 1
-    print(newnew)"""
+    data_ready = get_main("RX-78-2_Gundam")
+    #print(data_ready) 
 
     # store names of keys not to one-hot encode
     ohn_main = ['name', 'type', 'imgs']
     ohn_eps = ['_Television', '_OVA']
     ohn_measures = ['MassRatio', 'Height', 'Weight', \
         'Output', 'Length', 'Width', 'Range', 'Acceleration', 'Speed', 'TurningTime']
-    ohn_equiparms = ["Equipment", "Armaments"]
-    #print(ohn_main)
-    #print(ohn_measures)
-    #print(ohn_equiparms)
-    [print(key) for key in list(data_1mw.keys()) \
-        if any(ele in key for ele in ohn_main + ohn_eps + ohn_measures + ohn_equiparms)]
-    """for key in list(data_1mw.keys()):
-        if any(ele in key for ele in ohn_main + ohn_measures + ohn_equiparms):
-            print(key)"""
-
-    # for one-hot'ing shows specifically (since "first/last seen" is included \
-    # but useless for our purposes)
-    # # NEED TO INCORPORATE OVAs
-    """shows = []
-    [shows.append(i.split(":")[0]) for i in data_1mw["RealWorld_Television"]]
-    print(shows)"""
-
-    # joining
-    #new = new.join(newnew)
-    #print(new)
-    #new[0].join(new[0]str.join('|').str.get_dummies().add_prefix('tags_'))
-
+    ohn_equiparms = ["Equipment", "Armaments", "RocketThrusters"]
+    """[print(key) for key in list(data_1mw.keys()) \
+        if any(ele in key for ele in ohn_main + ohn_eps + ohn_measures + ohn_equiparms)]"""
+    
     # 2-level checker for equipment and armament: TOBE incorporated into counts
     """if ''.join(eq_std[0]) == eq_std[0]:
         #print('1 level!')
     else:
         #print("2 levels!")"""
+    
+    # One-Hot encoding: non-name or equipment features DONE
+    """spec_key = []
+    [spec_key.append(key) for key in list(data_1mw.keys()) \
+        if any(ele in key for ele in ohn_main + ohn_eps + ohn_measures + ohn_equiparms)]
+    onehot_main = np.setdiff1d(list(data_1mw.keys()), spec_key)
+    data_ready = [pd.concat([data_ready, one_hot(str(key))], axis = 1) for key in onehot_main]"""
+    #print(data_ready)
+    #print(data_ready.columns)
+
+    # for one-hot'ing shows specifically (since "first/last seen" is included \
+    # but useless for our purposes)
+    # # OVA INCORPORATED - ready TOBE Looped
+    """shows = []
+    [shows.append(i.split(":")[0]) for i in data_1mw["RealWorld_Television"]]
+    [shows.append(i.split(":")[0]) for i in data_1mw["RealWorld_OVA"] if "episode" in i]"""
+    #print(shows)    
+
